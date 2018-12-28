7a6d04691850b6e8286c0f4098c5bbd218ed678a
Trần Đạt <trandat180396@gmail.com>
2018-12-28 09:45:09 +0700

    Update code "show Shop interface"

---
+++ TableViewCell/TableViewCell/ItemCell.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+class ItemCell: UITableViewCell {
+    
+    @IBOutlet weak var iconImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
