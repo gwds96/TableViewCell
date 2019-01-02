64a0b9c393fe3dd8d9d7f558fb8101378294b80b
Trần Đạt <trandat180396@gmail.com>
2019-01-02 18:34:38 +0700

    Done with Main and List

--- TableViewCell/TableViewCell/Shop/FatherVC.swift
+++ TableViewCell/TableViewCell/Shop/FatherVC.swift
@@ -1,20 +1,45 @@
 import UIKit
 
 class FatherVC: UIViewController {
-
+    
+    @IBOutlet weak var button0: UIButton!
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var containerView: UIView!
+    var page: ShopPVC!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        button0.backgroundColor = UIColor.red
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let vc = segue.destination as? ShopPVC,
+            segue.identifier == "ShopPVC" {
+            self.page = vc
+            vc.delegateB = self
+        }
     }
     
     @IBAction func mainButton(_ sender: UIButton) {
-        let main = UIStoryboard(name: "Main", bundle: nil)
-        let page = main.instantiateViewController(withIdentifier: "ShopPageView") as! ShopPageView
-        page.inDex = 0
+        page.moveToPage(0)
     }
     
     @IBAction func listButton(_ sender: UIButton) {
-        let main = UIStoryboard(name: "Main", bundle: nil)
-        let page = main.instantiateViewController(withIdentifier: "ShopPageView") as! ShopPageView
-        page.inDex = 1
+        page.moveToPage(1)
+        
     }
+}
+
+extension FatherVC: ButtonDelegate {
+    func colorOfButton(_ number: Int) {
+        if number == 0 {
+            button1.backgroundColor = UIColor.white
+            button0.backgroundColor = UIColor.red
+        }else {
+            button0.backgroundColor = UIColor.white
+            button1.backgroundColor = UIColor.red
+        }
+    }
+    
+    
 }
---
+++ TableViewCell/TableViewCell/Shop/ShopPVC.swift
@@ -0,0 +1,67 @@
+import UIKit
+
+protocol ButtonDelegate: class {
+    func colorOfButton(_ number: Int)
+}
+
+class ShopPVC: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    var currentIndex: Int = 0
+    
+    weak var delegateB: ButtonDelegate?
+    
+    lazy var subViewControllers: [UIViewController] = {
+        return [
+            UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MainVC") as! MainVC,
+            UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "CatalogueVC") as! CatalogueVC
+        ]
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.dataSource = self
+        self.delegate = self
+        setViewControllers([subViewControllers[0]], direction: .forward, animated: true, completion: nil)
+    }
+    
+    func moveToPage(_ index: Int) {
+        if index == 1 {
+            setViewControllers([subViewControllers[index]], direction: .forward, animated: true, completion: nil)
+        }else {
+            setViewControllers([subViewControllers[index]], direction: .reverse, animated: true, completion: nil)
+        }
+        
+        func presentationCount(for pageViewController: UIPageViewController) -> Int {
+            return subViewControllers.count
+        }
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        currentIndex = subViewControllers.index(of: viewController) ?? 0
+        if currentIndex <= 0 {
+            return nil
+        }
+        currentIndex -= 1
+        return subViewControllers[currentIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        currentIndex = subViewControllers.index(of: viewController) ?? 0
+        if currentIndex >= subViewControllers.count - 1 {
+            return nil
+        }
+        currentIndex += 1
+        return subViewControllers[currentIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        if previousViewControllers == [subViewControllers[0]] {
+            currentIndex = 1
+        }else {
+            currentIndex = 0
+        }
+        delegateB?.colorOfButton(currentIndex)
+    }
+}
+
+
